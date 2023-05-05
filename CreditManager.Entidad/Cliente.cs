3c8aaba8155a4d4c53a7139ff2b16b4c48456667
juanpinedadev <juanpineda3000@gmail.com>
2023-05-05 10:37:03 -0500

    Se agregaron las clases Cliente y TipoDocumento en la capa Entidad

---
+++ CreditManager.Entidad/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CreditManager.Entidad
+{
+    public class Cliente
+    {
+        public int IdCliente { get; set; }
+        public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public string NumeroDocumento { get; set; }
+        public TipoDocumento TipoDocumento { get; set; }
+        public string NumeroTelefono { get; set; }
+        public string Direccion { get; set; }
+        public string NumeroCelular { get; set; }
+        public string CorreoElectronico { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public bool Estado { get; set; }
+    }
+}
---
+++ CreditManager.Entidad/TipoDocumento.cs
@@ -0,0 +1,8 @@
+﻿namespace CreditManager.Entidad
+{
+    public class TipoDocumento
+    {
+        public int IdTipoDocumento { get; set; }
+        public string Tipo { get; set; }
+    }
+}
