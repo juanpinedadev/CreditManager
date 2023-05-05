69f5a5671fb484490cb8d52ba7f4a4b88c01c250
juanpinedadev <juanpineda3000@gmail.com>
2023-05-05 10:39:09 -0500

    Se agregó la clase ConexionMaestra en la capa de Datos

---
+++ CreditManager.Datos/Conexion/ConexionMaestra.cs
@@ -0,0 +1,9 @@
+﻿using System.Configuration;
+
+namespace CreditManager.Datos.Conexion
+{
+    public class ConexionMaestra
+    {
+        public static string CadenaConexion = ConfigurationManager.ConnectionStrings["CadenaConexion"].ToString();
+    }
+}
