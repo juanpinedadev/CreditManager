031c986dbdd7696e17cc6b4b88f9c413ce2c5d41
juanpinedadev <juanpineda3000@gmail.com>
2023-05-05 11:14:07 -0500

    Se agregó el formulario Login

---
+++ CreditManager.Presentacion/Formulario/FormularioLogin.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace CreditManager.Presentacion.Formulario
+{
+    partial class FormularioLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(24, 172);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(237, 23);
+            this.txtUsuario.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(24, 226);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(237, 23);
+            this.txtContraseña.TabIndex = 2;
+            this.txtContraseña.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.label1.Location = new System.Drawing.Point(21, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ingrese el usuario:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.label2.Location = new System.Drawing.Point(21, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ingrese la contraseña:";
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(24, 261);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(237, 30);
+            this.btnIngresar.TabIndex = 5;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.label3.Location = new System.Drawing.Point(75, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Iniciar sesión";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(24, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(237, 71);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(80)))), ((int)(((byte)(80)))));
+            this.label4.Location = new System.Drawing.Point(90, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Credit Manager";
+            // 
+            // FormularioLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 311);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.txtUsuario);
+            this.Font = new System.Drawing.Font("Arial", 10F);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MinimumSize = new System.Drawing.Size(300, 350);
+            this.Name = "FormularioLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bienvenido";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormularioLogin_FormClosing);
+            this.Load += new System.EventHandler(this.FormularioLogin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtContraseña;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ CreditManager.Presentacion/Formulario/FormularioLogin.cs
@@ -0,0 +1,74 @@
+﻿using System.Windows.Forms;
+
+namespace CreditManager.Presentacion.Formulario
+{
+    public partial class FormularioLogin : Form
+    {
+        #region Constructor
+        public FormularioLogin()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Variables
+
+        #endregion
+
+        #region Eventos 
+        private void FormularioLogin_Load(object sender, System.EventArgs e)
+        {
+            txtUsuario.Focus();
+        }
+
+        private void btnIngresar_Click(object sender, System.EventArgs e)
+        {
+            ValidarCredenciales();
+        }
+
+        private void FormularioLogin_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.ApplicationExitCall) { return; }
+
+            DialogResult dialogResult = MessageBox.Show("¿Está seguro de que desea salir del sistema?",
+                "Atención", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            e.Cancel = (dialogResult == DialogResult.No);
+
+            if (!e.Cancel) { Application.Exit(); }
+        }
+        #endregion
+
+        #region Funciones
+        private void ValidarCredenciales()
+        {
+            if (txtUsuario.Text == "" && txtContraseña.Text == "")
+            {
+                MessageBox.Show("Los campos están vacíos", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtUsuario.Focus();
+            }
+
+            if (txtUsuario.Text == "")
+            {
+                MessageBox.Show("Proporcione un nombre de usuario.", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtUsuario.Focus();
+                return;
+            }
+
+            if (txtContraseña.Text == "")
+            {
+                MessageBox.Show("Proporcione una contraseña de usuario.", "Atención", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtContraseña.Focus();
+                return;
+            }
+
+            else
+            {
+                this.Hide();
+                //FormularioPrincipal dashboard = new FormularioPrincipal();
+                //dashboard.Show();
+            }
+        }
+        #endregion
+    }
+}
