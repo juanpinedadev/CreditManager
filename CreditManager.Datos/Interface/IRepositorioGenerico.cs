b9173db67f9176518b14711d41ef5e77bb44ffe3
juanpinedadev <juanpineda3000@gmail.com>
2023-05-05 10:41:58 -0500

    Se agregó la interface IRepositorioGenerico en la capa de Datos

---
+++ CreditManager.Datos/Interface/IRepositorioGenerico.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace CreditManager.Datos.Interface
+{
+    public interface IRepositorioGenerico<T> where T : class
+    {
+        int Crear(T entidad, out string mensaje);
+        bool Actualizar(T entidad, out string mensaje);
+        bool Eliminar(T entidad, out string mensaje);
+        List<T> Listar();
+    }
+}
