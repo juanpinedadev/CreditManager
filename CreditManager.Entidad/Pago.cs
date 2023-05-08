1c25fbca6bcaebfd0c0f4e5c982259b26f7fc986
juanpinedadev <juanpineda3000@gmail.com>
2023-05-08 18:54:30 -0500

    Se agregó la clase Pago y Prestamo

---
+++ CreditManager.Entidad/Pago.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CreditManager.Entidad
+{
+    public class Pago
+    {
+        public int IdPago { get; set; }
+        public Prestamo Prestamo { get; set; }
+        public decimal MontoAbonado { get; set; }
+        public DateTime FechaPago { get; set; }
+    }
+}
---
+++ CreditManager.Entidad/Prestamo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CreditManager.Entidad
+{
+    public class Prestamo
+    {
+        public int IdPrestamo { get; set; }
+        public Cliente Solicitante { get; set; }
+        public decimal MontoSolicitado { get; set; }
+        public decimal MontoTotalAPagar { get; set; } 
+        public int NumeroCuotas { get; set; } 
+        public decimal TasaInteres { get; set; } 
+        public DateTime FechaSolicitud { get; set; } 
+        public bool Pagado { get; set; }
+    }
+}
