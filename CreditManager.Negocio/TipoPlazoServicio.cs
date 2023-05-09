b76d0cb74555c3c64f0bf3dc78eb342b3ca8d8ff
juanpinedadev <juanpineda3000@gmail.com>
2023-05-09 12:59:46 -0500

    Se agregó la clase TipoPlazoServicio

---
+++ CreditManager.Negocio/TipoPlazoServicio.cs
@@ -0,0 +1,16 @@
+﻿using CreditManager.Datos.Repositorio;
+using CreditManager.Entidad;
+using System.Collections.Generic;
+
+namespace CreditManager.Negocio
+{
+    public class TipoPlazoServicio
+    {
+        private readonly TipoPlazoRepositorio tipoPlazoRepositorio = new TipoPlazoRepositorio();
+
+        public List<TipoPlazo> Listar()
+        {
+            return tipoPlazoRepositorio.Listar();    
+        }
+    }
+}
